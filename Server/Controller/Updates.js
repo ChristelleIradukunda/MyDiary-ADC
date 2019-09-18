9b0ba5955c8d984028d4c34d3cfbcae5251e8b69
ChristelleIradukunda <iradukundachristelle@akilahinstitute.org>
2019-09-18 07:08:00 +0300

    changing port and date format

--- Server/Controller/Updates.js
+++ Server/Controller/Updates.js
@@ -1,24 +1,25 @@
-import notes from '../Models/db'; 
+import notes from '../Models/db';
+import moment from 'moment';
 
-const UptoDate= (req, res) => {
-    let search = notes.find(item => item.id === parseInt(req.params.id))
-    if(search){
-        
-let igihe = {
+const UptoDate = (req, res) => {
+  let search = notes.find(item => item.id === parseInt(req.params.id))
+  if (search) {
+    search.title = req.body.title,
+      search.entry = req.body.entry
 
-    id: parseInt(req.body.id), 
-    title: req.body.title,
-    entry: req.body.entry, 
-    Date: new Date()
-  };
-
-  let DoIt = notes.indexOf(search);
-
-    notes.splice(DoIt, 1, igihe);
-
-    res.sendStatus(204);
+    return res.status(200).json({
+      status: 200,
+      message: "Successful",
+      data: {
+        id: search.id,
+        title: search.title,
+        entry: search.entry,
+        Date: moment().format('LL'),
+      }
+    })
   } else {
-    res.sendStatus(404);
-  }};
+    res.sendStatus(404).json;
+  }
+};
 
 export default UptoDate;
